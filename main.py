73c8776063b939b83873a1bae186c658062dbfb3
Chase <100978029+ChaseRocks360@users.noreply.github.com>
2024-07-20 23:22:36 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import pyautogui
+import time
+import keyboard
+
+def turn_90_degrees():
+    screen_width, screen_height, = pyautogui.size()
+
+    pyautogui.moveTo(screen_width / 2, screen_height / 2)
+
+    move_amount = screen_width / 4
+
+    pyautogui.moveRel(move_amount, 0, duration=0.1)
+
+while True:
+    if keyboard.is_pressed('F8'):
+        turn_90_degrees()
+        time.sleep(0.5)
+    
+    if keyboard.is_pressed('F7'):
+        print("F7 pressed. Killing Script.")
+        break
+
+    time.sleep(0.01)
